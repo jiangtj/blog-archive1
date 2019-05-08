d181e1bdf2df645c0b1b3fd8e49bf181463abcef
MrTT <jiang.taojie@foxmail.com>
2019-05-08 22:04:01 +0800

    Add lozad

--- scripts/views.js
+++ scripts/views.js
@@ -3,7 +3,8 @@
 'use strict';
 
 hexo.extend.filter.register('theme_inject', function(injects) {
-  injects.head.file('custom', 'source/_data/head.swig', {}, {cache: true});
-  injects.sidebar.file('custom', 'source/_data/sidebar.swig', {}, {cache: true});
+  injects.head.file('custom', 'views/head.swig', {}, {cache: true});
+  injects.sidebar.file('custom', 'views/sidebar.swig', {}, {cache: true});
   injects.bodyEnd.file('baidu-push', 'views/baidu-push.swig', {}, {cache: true});
+  injects.bodyEnd.file('lozad', 'views/lozad.swig', {}, {cache: true});
 });
