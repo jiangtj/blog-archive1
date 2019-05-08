3410b4d32755fce9fec1925d7dfba261fd7c9d89
MrTT <jiang.taojie@foxmail.com>
2019-05-08 20:45:35 +0800

    Remove baidu-push

--- scripts/views.js
+++ scripts/views.js
@@ -5,4 +5,5 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.head.file('custom', 'source/_data/head.swig', {}, {cache: true});
   injects.sidebar.file('custom', 'source/_data/sidebar.swig', {}, {cache: true});
+  injects.bodyEnd.file('baidu-push', 'views/baidu-push.swig', {}, {cache: true});
 });
