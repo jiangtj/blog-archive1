94dc90ae5e25db811f4d166601c6077feff4a71d
Mr.J <i@dnocm.com>
2019-06-12 14:23:30 +0800

    Moon menu (#56)
    * ....
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * ...
    
    * velocity?

--- scripts/views.js
+++ scripts/views.js
@@ -12,4 +12,35 @@ hexo.extend.filter.register('theme_inject', function(injects) {
   injects.variable.push('source/_data/variables.styl');
   injects.style.push('source/_data/styles.styl');
 
+  // inject moon-menu
+  hexo.theme.config.back2top.enable = false;
+  let moonMenu = Object.assign({
+    back2top: {
+      enable: true,
+      icon: 'fa fa-chevron-up',
+      func: 'back2top',
+      order: -1
+    },
+    back2bottom: {
+      enable: true,
+      icon: 'fa fa-chevron-down',
+      func: 'back2bottom',
+      order: -2
+    },
+  }, hexo.theme.config.moon_menu, hexo.config.moon_menu);
+  let moonMenuArr = Object.keys(moonMenu)
+    .map(key => moonMenu[key])
+    .map(item => {
+      item.order = item.order || 0;
+      if (item.enable === undefined) {
+        item.enable = true;
+      }
+      return item;
+    })
+    .filter(item => item.enable)
+    .sort((a, b) => a.order - b.order);
+  
+  injects.bodyEnd.file('moon-menu', 'views/moon-menu.swig', {menus: moonMenuArr}, {cache: true, only: true});
+  injects.style.push('views/moon-menu.styl')
+
 });
