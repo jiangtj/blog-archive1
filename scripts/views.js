87d10835f6ae254cf3377dbcfc32612e39e1d827
jiangtj <116749895@qq.com>
2019-07-03 18:27:39 +0800

    Add amp-auto-ads

--- scripts/views.js
+++ scripts/views.js
@@ -5,6 +5,7 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
 
   injects.head.file('custom', 'views/head.swig', {}, {cache: true});
+  injects.header.file('custom', 'views/header.swig', {}, {cache: true});
   injects.sidebar.file('custom', 'views/sidebar.swig', {}, {cache: true});
   injects.bodyEnd.file('baidu-push', 'views/baidu-push.swig', {}, {cache: true});
   injects.bodyEnd.file('lozad', 'views/lozad.swig', {}, {cache: true});
