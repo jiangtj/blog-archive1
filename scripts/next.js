40662f4452fea481cbba2d35ca65cfc609c1f338
jiangtj <116749895@qq.com>
2019-08-07 13:51:01 +0800

    Add next test script

---
+++ scripts/next.js
@@ -0,0 +1,21 @@
+/* global hexo */
+
+'use strict';
+
+if (hexo.config.theme !== 'next-sub') return;
+
+// 做一点兼容，方便测试NexT主题
+hexo.extend.filter.register('theme_inject', function(injects) {
+
+  injects.menu = new ViewInject();
+  hexo.theme.config.reward = null;
+
+}, -99);
+
+class ViewInject {
+  constructor() {
+    this.raws = [];
+  }
+  raw(name, raw, ...args) {}
+  file(name, file, ...args) {}
+}
