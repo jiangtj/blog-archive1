f386d13ea7c88c79ac33415426dddaa5ffa6f693
jiangtj <116749895@qq.com>
2019-08-08 11:38:01 +0800

    Update next sub

--- scripts/next.js
+++ scripts/next.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-if (hexo.config.theme !== 'next-sub') return;
+if (hexo.config.theme !== 'next') return;
 
 // 做一点兼容，方便测试NexT主题
 hexo.extend.filter.register('theme_inject', function(injects) {
