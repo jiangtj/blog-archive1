f9ad617a619bc2abaa7109301d1a18dad00d0354
MrTT <jiang.taojie@foxmail.com>
2019-08-15 23:18:33 +0800

    Format

--- source/downloads/code/hexo-newd.js
+++ source/downloads/code/hexo-newd.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 'use strict';
 
-function createNewPost(args){
-    console.log(args);
-    //需要将date改为正确的日期
-    args.slug = 'date-' + (args.slug||args._[0]);
-    hexo.call('new',args);
-};
+function createNewPost(args) {
+  console.log(args);
+  //需要将date改为正确的日期
+  args.slug = 'date-' + (args.slug || args._[0]);
+  hexo.call('new', args);
+}
 
 hexo.extend.console.register('newd', '为文件添加日期', {
-    arguments: [
-        { name: 'title', desc: '标题名' }
-    ]
+  arguments: [
+    { name: 'title', desc: '标题名' }
+  ]
 }, createNewPost);
