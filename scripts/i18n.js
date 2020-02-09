b292ceb80853144e4c9b0705b62c2f042a5f7b58
JiangTJ <116749895@qq.com>
2020-02-09 14:18:39 +0800

    ...

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -2,15 +2,14 @@
 
 'use strict';
 
-hexo.extend.filter.register('after_init', () => {
-  const injector = hexo.extend.injector2;
+const injector = require('hexo-extend-injector2')(hexo)
 
-  injector.register('postMeta', ({ __, post, i18n_post_meta }) => {
-    let metaContent = Object.keys(post.i18n).map(name => {
-      let link = post.i18n[name];
-      return i18n_post_meta(name, link, post.path);
-    });
-    return `
+injector.register('postMeta', ({ __, post, i18n_post_meta }) => {
+  let metaContent = Object.keys(post.i18n).map(name => {
+    let link = post.i18n[name];
+    return i18n_post_meta(name, link, post.path);
+  });
+  return `
     <span class="post-meta-item">
       <span class="post-meta-item-icon">
         <i class="fa fa-globe"></i>
@@ -18,11 +17,9 @@ hexo.extend.filter.register('after_init', () => {
       <span class="post-meta-item-text">${__('post.i18n')}</span>
       ${metaContent}
     </span>`;
-  }, ctx => ctx.post.i18n);
+}, ctx => ctx.post.i18n);
 
-});
-
-hexo.extend.helper.register('i18n_post_meta', function(name, link, path) {
+hexo.extend.helper.register('i18n_post_meta', function (name, link, path) {
   link = this.url_for(link).replace('index.html', '');
   path = this.url_for(path).replace('index.html', '');
   let postMeta = `<span>${name}</span>`;
