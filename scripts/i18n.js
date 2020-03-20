1ef159b305224e13b20c5c4706219679906cb80f
MrTT <jiang.taojie@foxmail.com>
2020-03-20 16:25:35 +0800

    u theme

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -2,7 +2,8 @@
 
 'use strict';
 
-const injector = require('hexo-extend-injector2')(hexo)
+const injector = require('hexo-extend-injector2')(hexo);
+const fa = hexo.extend.helper.get('fa_inline').bind(hexo);
 
 injector.register('postMeta', ({ __, post, i18n_post_meta }) => {
   let metaContent = Object.keys(post.i18n).map(name => {
@@ -12,6 +13,7 @@ injector.register('postMeta', ({ __, post, i18n_post_meta }) => {
   return `
     <span class="post-meta-item">
       <span class="post-meta-item-icon">
+        ${fa('globe', {prefix: 'fas'})}
         <i class="fa fa-globe"></i>
       </span>
       <span class="post-meta-item-text">${__('post.i18n')}</span>
@@ -19,7 +21,7 @@ injector.register('postMeta', ({ __, post, i18n_post_meta }) => {
     </span>`;
 }, ctx => ctx.post.i18n);
 
-hexo.extend.helper.register('i18n_post_meta', function (name, link, path) {
+hexo.extend.helper.register('i18n_post_meta', function(name, link, path) {
   link = this.url_for(link).replace('index.html', '');
   path = this.url_for(path).replace('index.html', '');
   let postMeta = `<span>${name}</span>`;
